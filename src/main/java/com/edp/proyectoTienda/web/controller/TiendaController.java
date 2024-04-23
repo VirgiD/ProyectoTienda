9c9b86c5760f09db3d9fc97fbce954c796e78319
Virgi D <virginiadanitz@gmail.com>
2024-04-23 16:34:00 -0300

    proyecto tienda

---
+++ src/main/java/com/edp/proyectoTienda/web/controller/TiendaController.java
@@ -0,0 +1,21 @@
+package com.edp.proyectoTienda.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/inicio")
+public class TiendaController {
+
+    @GetMapping ("/holamundo")
+    public String HolaMundo(){
+     return "hola mundo";
+
+    }
+
+    @GetMapping ("")
+    public String getInit(){
+    return "Inicio";
+    }
+}
